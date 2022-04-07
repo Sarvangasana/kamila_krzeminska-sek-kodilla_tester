8ece7e1a07c126437479b8aaaeec2581d5c4823a
Kamila Krzeminska-Sek <krzeminska.kamila@gmail.com>
2022-04-07 16:17:55 +0200

    Main + MainExtended 07.04.2022

---
+++ kodilla_intro/src/main/java/MainExtended.java
@@ -0,0 +1,14 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myProffesion = "Tester";
+        int yearsOfExp = 0;
+        double weight = 57.5;
+        char DrivingLicenceCat = 'B';
+        boolean isGoodYogi = true;
+        String surname = "Krzemińska-Sęk";
+
+        System.out.println(surname + ' ' + weight + ' ' + isGoodYogi);
+        System.out.println(myProffesion + ' ' + yearsOfExp + ' ' + DrivingLicenceCat);
+
+    }
+}
