cb2b772d231d90bdb3b848989c82cff00f328634
Kamila Krzeminska-Sek <krzeminska.kamila@gmail.com>
2022-04-07 15:44:08 +0200

    First Class, Hello World 07.04.2022

---
+++ kodilla_intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Kamila");
+        System.out.println("34 wiosny");
+        System.out.println("K-S");
+    }
+}
