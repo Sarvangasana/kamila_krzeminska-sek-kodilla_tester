02c54b3a4680bc32ab9a828da2e923a8b9c1359c
Kamila Krzeminska-Sek <krzeminska.kamila@gmail.com>
2022-06-17 13:52:48 +0200

    Task 2.7. Book.of() - poprawione

--- m_2_object_orientation/src/main/java/Book.java
+++ m_2_object_orientation/src/main/java/Book.java
@@ -2,11 +2,6 @@ public class Book {
     private String author;
     private String title;
 
-    //    private Book(String author, String title) {
-//        this.author = author;
-//        this.title = title;
-//    }
-//
 //    static Book of(String author, String title) {
 //        return new Book(author, title);
 //    }
