35a1fdf3ae5001d74cd9f64503ed19de2a614288
Kamila Krzeminska-Sek <krzeminska.kamila@gmail.com>
2022-07-26 18:20:40 +0200

    Task 7.2.

---
+++ m_7_kodilla_advaced_tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class GamblingMachineTestSuite {
+
+    private GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/intUserNumbers.csv")
+    public void shouldCountHowManyWins(int element1, int element2, int element3, int element4, int element5,
+                                       int element6) throws InvalidNumbersException {
+        Set<Integer> integerSet = new HashSet<>();
+        integerSet.add(element1);
+        integerSet.add(element2);
+        integerSet.add(element3);
+        integerSet.add(element4);
+        integerSet.add(element5);
+        integerSet.add(element6);
+
+        gamblingMachine.howManyWins(integerSet);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/setIntegerInvalidUserNumbers.csv")
+        public void shouldThrowException(int element1, int element2, int element3, int element4, int element5,
+                                         int element6) throws InvalidNumbersException {
+            Set<Integer> integerSet = new HashSet<>();
+            integerSet.add(element1);
+            integerSet.add(element2);
+            integerSet.add(element3);
+            integerSet.add(element4);
+            integerSet.add(element5);
+            integerSet.add(element6);
+
+            gamblingMachine.howManyWins(integerSet);
+   }
+
+}
