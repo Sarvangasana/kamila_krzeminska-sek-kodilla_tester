3899fea6dd032b3de901626e76515fb023687d70
Kamila Krzeminska-Sek <krzeminska.kamila@gmail.com>
2022-08-21 00:21:39 +0200

    Task 13.4. poprawione

--- m_13_kodilla_selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ m_13_kodilla_selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllegroTestingApp {
     public static void main(String[] args) throws InterruptedException {
@@ -11,22 +13,17 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl/");
 
-        Thread.sleep(10000);
-        driver.findElement(By.xpath("//*[@id=\"opbox-gdpr-consents-modal\"]/div/div/div/div[2]/button")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
 
-        Thread.sleep(10000);
-////        PYTANIE: Jak zastosować prawidłowo - zamiast thread.sleep - opcję z wait.until(ExpectedConditions)??
-//        Jak próbuję wg modelu poniżej, to podkreśla mi na czerwono
-////        WebDriverWait wait = new WebDriverWait(driver, 20);
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
-//                "//*[@aria-label=\"Kategoria i opcje wyszukiwania\"]/optgroup/option[3]")));
-        driver.findElement(By.xpath("//*[@aria-label=\"Kategoria i opcje wyszukiwania\"]/optgroup/option[3]")).click();
+        WebElement gdprConsentButton = driver.findElement(By.xpath("//*[@id=\"opbox-gdpr-consents-modal\"]/div/div/div/div[2]/button"));
+        wait.until(ExpectedConditions.visibilityOf(gdprConsentButton)).click();
 
-        Thread.sleep(10000);
+        WebElement electronicsSearchOption = driver.findElement(By.xpath("//*[@aria-label=\"Kategoria i opcje wyszukiwania\"]/optgroup/option[3]"));
+        wait.until(ExpectedConditions.visibilityOf(electronicsSearchOption)).click();
+
         WebElement inputField = driver.findElement(By.xpath("//*[@data-role=\"search-input\"]"));
-        Thread.sleep(10000);
+        wait.until(ExpectedConditions.visibilityOf(inputField));
         inputField.sendKeys("mavic mini");
-        Thread.sleep(10000);
         inputField.submit();
     }
 }
